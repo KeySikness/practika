b5db5912b780ca2dd04161e0c5a17208ec053e93
KeySikness <krissskichanova9@gmail.com>
2025-07-04 16:39:59 +1000

    0-0

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
+import pygame as py
+import sys
+import os
+
+def main():
+    py.init()
+
+    screen_size = (800, 600)
+    screen = py.display.set_mode(screen_size, py.RESIZABLE)
+    py.display.set_caption("game")
+
+    clock = py.time.Clock()
+
+    running = True
+    while running:
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                running = False
+            elif event.type == py.VIDEORESIZE:
+                screen_size = event.size
+                screen = py.display.set_mode(screen_size, py.RESIZABLE)
+
+        py.display.flip()
+        clock.tick(60)
+
+    py.quit()
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
+
 #тут кароче игра лол
 
 # 　　　　　／＞　 フ
